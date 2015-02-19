163e46f0a0cdf3c20be46d9533c6361b6634dd07
herve92 <Main@129.194.245.203>
2015-02-19 17:32:25 +0100

    first

---
+++ testmaven/src/testmaven/Main.java
@@ -0,0 +1,18 @@
+package testmaven;
+
+
+
+public class Main {
+
+	public static void main(String[] args){
+		System.out.println("Hello");
+		
+	}
+	
+	public Main(){
+		//MailEvent mailEvent1 = null;
+	}
+	
+	
+	}
+	
